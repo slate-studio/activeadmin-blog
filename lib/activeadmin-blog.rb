a5b8fb2adfdaf6a61a01a67ec9b9592182eccf53
Alex Kravets <santyor@gmail.com>
2012-08-11 18:21:00 +0300

    fixes;

--- lib/activeadmin-blog.rb
+++ lib/activeadmin-blog.rb
@@ -3,6 +3,7 @@ require "mongoid_slug"
 require "mongoid_search"
 require "activeadmin-mongoid"
 require "activeadmin-mongoid-reorder"
+require "activeadmin-settings"
 
 require "activeadmin-blog/version"
 require "activeadmin-blog/engine"
