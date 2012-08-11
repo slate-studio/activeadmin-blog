96acfe54cfbdd3f1c938c7a07e9564bb7dbbdf95
Alex Kravets <santyor@gmail.com>
2012-08-11 17:46:26 +0300

    fixes;

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActiveadminBlog::Engine.routes.draw do
   get '/'                 => 'posts#index',     :as => :blog
-  get '/:slug'            => 'posts#category',  :as => :blog_category
   get '/search'           => 'posts#search',    :as => :blog_search
+  get '/:slug'            => 'posts#category',  :as => :blog_category
   get '/archive/:y/:m'    => 'posts#archive',   :as => :blog_archive
   get '/tags/:tag'        => 'posts#tag',       :as => :blog_tag
   get '/posts/:slug'      => 'posts#show',      :as => :blog_post
